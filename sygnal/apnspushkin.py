b139556389eb23085903f51324a13ac229e23f00
Yevhenii Onipko <y.onipko@kyivstar.net>
2021-09-23 20:02:08 +0300

    :construction: - Refactor

--- sygnal/apnspushkin.py
+++ sygnal/apnspushkin.py
@@ -457,9 +457,9 @@ class ApnsPushkin(ConcurrencyLimitedPushkin):
 
         if n.type == "m.room.message":
             if n.content and "body" in n.content:
-                payload["aps"]["alert"] = n.content["body"]
-#             else:
-#                 payload["aps"]["alert"] = "You have a new message"
+                payload["aps"]["alert"] = n.content["body"][:65]
+            else:
+                payload["aps"]["alert"] = "You have a new message"
 
         # if loc_key:
         #     payload["aps"].setdefault("alert", {})["loc-key"] = loc_key
