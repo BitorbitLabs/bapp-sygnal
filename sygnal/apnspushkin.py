afc89101df485b030d1f6373569dafc632fe4dcc
Yevhenii Onipko <y.onipko@kyivstar.net>
2021-09-23 19:29:11 +0300

    :construction: - Refactor

--- sygnal/apnspushkin.py
+++ sygnal/apnspushkin.py
@@ -455,11 +455,17 @@ class ApnsPushkin(ConcurrencyLimitedPushkin):
 
         payload.setdefault("aps", {})
 
-        if loc_key:
-            payload["aps"].setdefault("alert", {})["loc-key"] = loc_key
+        if n.type == "m.room.message"
+            if n.content and "body" in n.content:
+                payload["aps"]["alert"] = n.content["body"]
+            else:
+                payload["aps"]["alert"] = "You have a new message"
 
-        if loc_args:
-            payload["aps"].setdefault("alert", {})["loc-args"] = loc_args
+        # if loc_key:
+        #     payload["aps"].setdefault("alert", {})["loc-key"] = loc_key
+        #
+        # if loc_args:
+        #     payload["aps"].setdefault("alert", {})["loc-args"] = loc_args
 
         if badge is not None:
             payload["aps"]["badge"] = badge
