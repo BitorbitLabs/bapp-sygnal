da31a79f0934d11f491f5174f7775692be9f7e91
Yevhenii Onipko <y.onipko@kyivstar.net>
2021-10-04 18:46:36 +0300

    :construction: - Refactor

--- sygnal/apnspushkin.py
+++ sygnal/apnspushkin.py
@@ -458,7 +458,7 @@ class ApnsPushkin(ConcurrencyLimitedPushkin):
         if n.type == "m.room.message":
             if n.content and "body" in n.content:
                 if n.content["body"]:
-                    payload["aps"]["alert"]["body"] = n.content["body"][:65];
+                    payload["aps"]["alert"]["body"] = n.content["body"][:65]
                     if "sender_display_name" in n:
                         payload["aps"]["alert"]["title"] = n["sender_display_name"]
                 else:
